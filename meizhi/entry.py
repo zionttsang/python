142b803aa6b2cd1cf5d36b97bf84815d4a4069b3
Tsang <schwarzen221@gmail.com>
2017-01-03 17:26:38 +0800

    2016/01/03

--- meizhi/entry.py
+++ meizhi/entry.py
@@ -1,3 +1,16 @@
 
-from scrapy.cmdline import execute
-execute(["scrapy","crawl","meizhi"])
+# from scrapy.cmdline import execute
+# execute(["scrapy","crawl","meizhi"])
+
+# import requests
+# from requests.packages import urllib3 
+
+# urllib3.disable_warnings()
+# response = requests.get("https://www.12306.cn",verify = False)
+# print (response.status_code)
+
+from urllib import request
+
+req = request.Request('http://www.baidu.com/')
+res = request.urlopen(req)
+print ("res code:",res.getcode())
--- meizhi/meizhi/spiders/spider.py
+++ meizhi/meizhi/spiders/spider.py
@@ -3,18 +3,50 @@ import scrapy
 from bs4 import BeautifulSoup
 from scrapy.http import Request
 from meizhi.items import MeizhiItem
+import random
+# import meizhi.zhihuLogIn
 
 class Spider(scrapy.Spider):
-	name = "meizhi"
-	allow_domains = ["https://www.baidu.com/"]
-	# start_url_head = "https://www.zhihu.com/collection/38624707?page="
+	# userAgentList = ["Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+ # "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+ # "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+ # "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+ # "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+ # "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+ # "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+ # "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+ # "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+ # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+ # "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+ # "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+ # "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+ # "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+ # "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+ # "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+ # "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+ # "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+	# ]
+	# ua = random.choice(userAgentList)
 	
-	def start_requests_pages(self):
+	headers = {
+    "Host": "www.zhihu.com",
+    "Referer": "https://www.zhihu.com/",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36	'
+	}
+
+	name = "meizhi"
+	allow_domains = ["https://www.zhihu.com/"]
+	start_url_head = "https://www.zhihu.com/collection/38624707?page=1"
+	
+	def start_requests(self):
+	
+		yield Request("https://www.zhihu.com/collection/38624707?page=1",callback = self.parse_pages,headers = self.headers)
+		
 		# for i in range(1,1):
 			# url = self.start_url_head + str(i)
 			# yield Request(url,self.parse)
-		yield Request("https://www.baidu.com/",self.parse)
 	
-	def parse(self,response):
-		print(response.text)
-		
+	def parse_pages(self,response):
+		print('responseStatus:',response.status)
+		filename = 'testFilename'
+		self.log('saved file %s' %filename)
