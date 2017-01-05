e6e4547b12d71722f8a92622c0dc386f086dd9f1
Tsang <schwarzen221@gmail.com>
2017-01-05 17:31:41 +0800

    2017/01/05
    scrapy after login

---
+++ meizhi/meizhi/__init__.py
@@ -0,0 +1,2 @@
+
+# import meizhi.zhihuLogIn
--- meizhi/meizhi/spiders/spider.py
+++ meizhi/meizhi/spiders/spider.py
@@ -4,49 +4,104 @@ from bs4 import BeautifulSoup
 from scrapy.http import Request
 from meizhi.items import MeizhiItem
 import random
-# import meizhi.zhihuLogIn
+
+import meizhi.zhihuLogIn
+from meizhi.zhihuLogIn import isLogin,login
+import requests
+try:
+	import cookielib
+except:
+	import http.cookiejar as cookielib
+import time
+import os.path
+try:
+	from PIL import Image
+except:
+	pass
 
 class Spider(scrapy.Spider):
-	# userAgentList = ["Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
- # "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
- # "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
- # "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
- # "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
- # "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
- # "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
- # "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
- # "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
- # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
- # "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
- # "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
- # "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
- # "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
- # "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
- # "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
- # "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
- # "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
-	# ]
-	# ua = random.choice(userAgentList)
 	
+	# 构造 Request headers
+	agent = 'Mozilla/5.0 (Windows NT 5.1; rv:33.0) Gecko/20100101 Firefox/33.0'
 	headers = {
-    "Host": "www.zhihu.com",
-    "Referer": "https://www.zhihu.com/",
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36	'
+		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+		"Accept-Encoding": "gzip,deflate",
+		"Accept-Language": "en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4",
+		"Connection": "keep-alive",
+		"Content-Type":" application/x-www-form-urlencoded; charset=UTF-8",
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/48.0.2564.97 Safari/537.36",
+		"Referer": "http://www.zhihu.com"
 	}
+
+	# 使用登录cookie信息
+	session = requests.session()
+	session.cookies = cookielib.LWPCookieJar(filename='cookies')
+	try:
+		session.cookies.load(ignore_discard=True)
+	except:
+		print("Cookie 未能加载")
 
 	name = "meizhi"
 	allow_domains = ["https://www.zhihu.com/"]
 	start_url_head = "https://www.zhihu.com/collection/38624707?page=1"
 	
-	def start_requests(self):
-	
-		yield Request("https://www.zhihu.com/collection/38624707?page=1",callback = self.parse_pages,headers = self.headers)
+	# def __init__(self, *args, **kwargs):
+		# super(Spider, self).__init__(*args, **kwargs)
+		# self.xsrf = ''
 		
-		# for i in range(1,1):
-			# url = self.start_url_head + str(i)
-			# yield Request(url,self.parse)
-	
-	def parse_pages(self,response):
-		print('responseStatus:',response.status)
-		filename = 'testFilename'
-		self.log('saved file %s' %filename)
+	def start_requests(self):
+		if isLogin():
+			print ('you have already loged in')
+			yield meizhi.zhihuLogIn.session.get(self.start_url_head, self.after_login)
+
+		else:
+			account = input('请输入你的用户名\n>  ')
+			secret = input("请输入你的密码\n>	")
+			login(secret, account)
+			print ('ok,now you are loged in')
+			
+			yield session.get(self.start_url_head, callback = self.after_login)
+			
+		
+	def after_login(self, response):
+		pirnt('you are in the after_login')
+
+		
+	def parse_firstpage(self,response):
+		print ('get in the parse_firstpage')
+		exit(0)
+		
+		
+	def parse_err(self, response):
+		log.ERROR('crawl {} failed'.format(response.url))
+		
+	def get_xsrf(self):
+		'''_xsrf 是一个动态变化的参数'''
+		index_url = 'https://www.zhihu.com'
+		# 获取登录时需要用到的_xsrf
+		index_page = session.get(index_url, headers=headers)
+		html = index_page.text
+		pattern = r'name="_xsrf" value="(.*?)"'
+		# 这里的_xsrf 返回的是一个list
+		_xsrf = re.findall(pattern, html)
+		return _xsrf[0]
+
+
+	# 获取验证码
+	def get_captcha(self):
+		t = str(int(time.time() * 1000))
+		captcha_url = 'https://www.zhihu.com/captcha.gif?r=' + t + "&type=login"
+		r = session.get(captcha_url, headers=headers)
+		with open('captcha.jpg', 'wb') as f:
+			f.write(r.content)
+			f.close()
+		# 用pillow 的 Image 显示验证码
+		# 如果没有安装 pillow 到源代码所在的目录去找到验证码然后手动输入
+		try:
+			im = Image.open('captcha.jpg')
+			im.show()
+			im.close()
+		except:
+			print(u'请到 %s 目录找到captcha.jpg 手动输入' % os.path.abspath('captcha.jpg'))
+		captcha = input("please input the captcha\n>")
+		return captcha
