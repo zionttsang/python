c7768e840bb8e309794f15d0dcc9f83c829c96ac
Tsang <schwarzen221@gmail.com>
2017-06-06 16:41:28 +0800

    turn to requests

--- Pure-Spider/main_brain.py
+++ Pure-Spider/main_brain.py
@@ -3,41 +3,37 @@
 from urllib.request import urlopen
 from urllib.error import URLError
 from bs4 import BeautifulSoup  
+import requests
 import os  
 import re	
-from zhihuLogIn import isLogin,login
+# from zhihuLogIn import isLogin,login
 from globalClass import *
 import time
 
 #urlFirst = "https://www.zhihu.com/collection/60771406?page=1"#指定的URL
 count = globalClass()
-  
-def download(_url,name):#下载函数  
-	if(_url==None):#地址若为None则跳过	 
-		pass  
-	resDown = urlopen(_url)#打开链接  
-	# time.sleep(0.25) # 时间间隔为0.5s发送一次抓取请求，减轻hoj服务器压力
-	if (resDown.getcode() == 200):
-		data=resDown.read()#否则开始下载到本地  
-		resDown.close()
-		with open(name, "wb") as code:
-			code.write(data)
-			code.close()
-	else:
-		print("resDown get error!")
-		resDown.close()
 
-def AfterLogIn(url):
+agent = 'Mozilla/5.0 (Windows NT 5.1; rv:33.0) Gecko/20100101 Firefox/33.0'
+headers = {
+    "Host": "www.zhihu.com",
+    "Referer": "https://www.zhihu.com/",
+    'User-Agent': agent
+}
 
-	print('start AfterLogIn')
+def GetAllPics(url):
+
+	print('Get in Brain')
 	
-	resPage = urlopen(url)#打开目标地址  
-	respond=resPage.read()#获取网页地址源代码  
-	resPage.close()
+	# resPage = urlopen(url)#打开目标地址  
+	# print("status code: ",resPage.getcode)
+	# respond=resPage.read()#获取网页地址源代码  
+	# resPage.close()
 
+	respond = requests.post(url, headers = headers)
+	print("status code: ", respond.status_code)
 	soup=BeautifulSoup(respond,"lxml")#实例化一个BeautifulSoup对象	 
 	lstImg = []#Create listImg Obj
-	  
+		
 	print("prepare for the loop")
 	
 	for link in soup.find_all("link",href = re.compile("answer")):#获取标签为link的内容	
@@ -66,15 +62,31 @@ def AfterLogIn(url):
 			addressImg = linkImg.get("data-original")
 			lstImg.append(addressImg)
 
-	s = set(lstImg)
+	lstThisPageSrcs = set(lstImg)
 	
 
-	for address in s:  
+	for address in lstThisPageSrcs:  
 		if(address!=None):	
 			pathName="E:\\2666\\"+ str(count.z) +".jpg"#设置路径和文件名	
 			download(address,pathName)#下载 
 			print ("正在下载第：",count.z)			
 			count.z=count.z+1#计数君+1
+
+
+def download(_url,name):#下载函数  
+	if(_url==None):#地址若为None则跳过	 
+		pass  
+	resDown = urlopen(_url)#打开链接  
+	# time.sleep(0.25) # 时间间隔为0.5s发送一次抓取请求，减轻hoj服务器压力
+	if (resDown.getcode() == 200):
+		data=resDown.read()#否则开始下载到本地  
+		resDown.close()
+		with open(name, "wb") as code:
+			code.write(data)
+			code.close()
+	else:
+		print("resDown get error!")
+		resDown.close()
 			
 
 import os, sys
