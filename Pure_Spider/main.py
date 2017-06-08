2b295d6f2b375b94fe835e500aac12d9c1d2580f
Tsang <schwarzen221@gmail.com>
2017-06-08 17:30:41 +0800

    reduce_argvs_undone

--- Pure_Spider/main.py
+++ Pure_Spider/main.py
@@ -4,39 +4,50 @@ import main_brain
 from multiprocessing import Process, Lock
 import string
 import global_class
+from zhihu_class import *
+import sys
 
-class main():
-	def __init__(self, collectionUrl, pageNum):
-	# urlFirst = "https://www.zhihu.com/collection/60771406?page=" #daxiong
-		self.collectionUrl = collectionUrl
-		self.pageNum = pageNum
+# class main():
+# 	def __init__(self, collectionUrl, pageNum):
+# 	# urlFirst = "https://www.zhihu.com/collection/60771406?page=" #daxiong
+# 		self.collectionUrl = collectionUrl
+# 		self.pageNum = pageNum
+cZhi = zhihu_class()
 
-	def main(self):
+def main(collectionUrl, pageNum):
+	# check page tail
+	if cZhi.IsPageTailExist(collectionUrl) == False:
+		print("Wrong Collection Url")
+		exit()
+	pageTail = "?page="
+	urlCollectionWithPageTail = collectionUrl + pageTail
+	print(urlCollectionWithPageTail)
 
-		# print("Page/s: ", int(pageNum / 10))
-		for i in range(int(self.pageNum / 10) + 1):
-			p = Process(target = self.SingleProcess, args = (i,))
-			p.start()
+	# check total page num
+	totalPageNum = cZhi.GetTotalPageNum(collectionUrl)
 
-	def SingleProcess(self, i):
-		print("Process Num: ", i)
-		for j in range(10):
-			if i != 0:
-				intPage = str((i*10) + j)
-				urlThisPage = self.collectionUrl + str((i*10) + j)
-			else:
-				if j == 9:
-					continue
-				intPage = str(j + 1)
-				urlThisPage = self.collectionUrl + str(j + 1)
+	for i in range(int((int(pageNum) / 10) + 1)):
+		p = Process(target = SingleProcess, args = (urlCollectionWithPageTail, i,))
+		p.start()
 
-			print("urlThisPage: ",urlThisPage)
-			# continue
-			main_brain.GetSinglePageAllPics(urlThisPage, intPage)
-			# time.sleep(1)
+def SingleProcess(collectionUrl, i):
+	print("Process Num: ", i)
+	for j in range(10):
+		if i != 0:
+			intPage = str((i*10) + j)
+			urlThisPage = collectionUrl + str((i*10) + j)
+		else:
+			if j == 9:
+				continue
+			intPage = str(j + 1)
+			urlThisPage = collectionUrl + str(j + 1)
+
+		print("urlThisPage: ",urlThisPage)
+		# continue
+		cZhi.GetSinglePageAllPics(urlThisPage, intPage)
+		# time.sleep(1)
 		
 if __name__ == '__main__':
-	collectionUrl = "https://www.zhihu.com/collection/38624707?page=" #baozhao
-	pageNum = 67
-	cMain = main(collectionUrl, pageNum)
-	cMain.main()
+
+	main(sys.argv[1], sys.argv[2])
+	# print("sys.argv: ",sys.argv[1], sys.argv[2])
--- Pure_Spider/zhihu_class.py
+++ Pure_Spider/zhihu_class.py
@@ -1,16 +1,41 @@
 # zhihu_class.py
 from global_class import *
+# import global_class
 from bs4 import BeautifulSoup  
 import re
 from urllib.error import URLError
 import time
-
+import lxml
 class zhihu_class:
 	def __init__(self):
 		self.glb = global_class()
+		self.headers = {
+    "Host": "www.zhihu.com",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+		}
+
+
+	def GetSinglePageAllPics(self, url, intPage):
+
+		strPathNameHeader = self.glb.GetSysPlat()
+		
+		# print("self.headers: ", glb.headers)
+		lstThisPageSrcs = self.GetSinglePageCollectionSrc(url)	
+
+		for address in lstThisPageSrcs:  
+			if(address != None):	
+				strName = str(intPage) + "_" + str(self.glb.count)
+				strFullPath = strPathNameHeader + strName +".jpg"
+				# print("strFullPath: ", strFullPath)
+				# exit()
+				#设置路径和文件名	
+				print ("正在下载: ", strName)
+				self.glb.DownLoadInBinary(address,strFullPath)#下载 
+				self.glb.count = self.glb.count + 1#计数君+1
+
 
 	def GetSinglePageCollectionSrc(self, collectionPageUrl):
-		req = requests.request("GET", collectionPageUrl, headers = self.glb.headers)
+		req = requests.request("GET", collectionPageUrl, headers = self.headers)
 		if req.status_code != 200:
 			print("bad req in single page: ", req.status_code)
 			return False
@@ -31,7 +56,7 @@ class zhihu_class:
 			print ("adressLinkFull: ",addressLinkFull) 
 				
 			try:
-				reqAnswer = requests.request("GET", addressLinkFull, headers = self.glb.headers)
+				reqAnswer = requests.request("GET", addressLinkFull, headers = self.headers)
 				time.sleep(1)
 			except URLError as e:
 				print("bad path!!")
@@ -47,3 +72,20 @@ class zhihu_class:
 				lstImg.append(addressImg)
 
 		return set(lstImg)
+
+	def IsPageTailExist(self, urlCollection):
+		if urlCollection.find("page") == -1:
+			return True
+		else:
+			return False
+
+	def GetTotalPageNum(self, urlCollection):
+		req = requests.get(urlCollection, headers = self.headers)
+		soup = BeautifulSoup(req.content, "html")
+		html = lxml.etree.parse(soup)
+		nodeLastPage = html.xPath('<a href="?page=28">28</a>')
+		totalPageNum = nodeLastPage.Text
+		print("totalPageNum: ", totalPageNum)
+		exit()
+		return totalPageNum
+
